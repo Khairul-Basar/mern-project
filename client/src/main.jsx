d27fbeed064bce2da20707f2e128220ce0248734
Khairul-Basar <khairulbasar130@gmail.com>
2023-12-08 19:22:35 +0600

    added axios and make a function for all notes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.withCredentials = true;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+);
