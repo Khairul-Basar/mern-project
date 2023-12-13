eb201860e76ffd5651ca71b6a814c43eeb3df50b
Khairul-Basar <khairulbasar130@gmail.com>
2023-12-13 22:48:13 +0600

    Implemented connet between note and user. who made note they can see the note

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -2,20 +2,22 @@ const Note = require("../models/noteSchema");
 
 const allNotes = async (req, res) => {
   try {
-    const notes = await Note.find();
-    res.json(notes);
+    const notes = await Note.find({ user: req.user._id });
+    res.json({ notes });
   } catch (err) {
     console.log(err);
+    res.sendStatus(400);
   }
 };
 
 const singleNote = async (req, res) => {
   const { id } = req.params;
   try {
-    const notes = await Note.findById(id);
+    const notes = await Note.findOne({ _id: id, user: req.user._id });
     res.json(notes);
   } catch (err) {
     console.log(err);
+    res.sendStatus(400);
   }
 };
 
@@ -26,10 +28,12 @@ const creatNote = async (req, res) => {
     const note = await Note.create({
       title,
       description,
+      user: req.user._id,
     });
     res.json(note);
   } catch (err) {
     console.log(err);
+    res.sendStatus(400);
   }
 };
 
@@ -37,26 +41,34 @@ const updateNote = async (req, res) => {
   try {
     const { title, description } = req.body;
     const { id } = req.params;
-    await Note.findByIdAndUpdate(id, {
-      title,
-      description,
-    });
+    await Note.findOneAndUpdate(
+      {
+        _id: id,
+        user: req.user._id,
+      },
+      {
+        title,
+        description,
+      }
+    );
     const note = await Note.findById(id);
     res.json(note);
   } catch (err) {
     console.log(err);
+    res.sendStatus(400);
   }
 };
 
 const deleteNote = async (req, res) => {
   try {
     const { id } = req.params;
-    await Note.deleteOne({ _id: id });
+    await Note.deleteOne({ _id: id, user: req.user._id });
     res.json({
       success: "Note Delete Successfully...!!",
     });
   } catch (err) {
     console.log(err);
+    res.sendStatus(400);
   }
 };
 
